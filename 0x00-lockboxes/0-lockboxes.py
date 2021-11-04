1848aaa7ea3d63bdf40984d6e8e28b113dfcc7e7
rkbrian <rkbriankong@gmail.com>
2021-11-04 07:15:10 +0000

    debug: wrong order

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -9,20 +9,20 @@ def canUnlockAll(boxes):
     """ make list of locked boxes in rep of numbers """
     for i in range(len(boxes)):
         lobo = lobo | {i}
-    while len(keyly) <= len(boxes):
+    while len(keyly) < len(boxes):
         """ collect all the keys from the box that match the current key """
         for i in range(len(boxes[keycur])):
-            if boxes[keycur][i] in (lobo | keyly):
+            if boxes[keycur][i] in lobo:
                 keyly = keyly | {boxes[keycur][i]}
         """ keyly = set(keyly | {0}) no dup """
         boxes[keycur] = []
         """ search for the next useful key """
+        lobo = lobo - {keycur}
+        if len(lobo) == 0:
+            return True
         if len(keyly & lobo) > 0:
             keycur = min((keyly & lobo))
             """ print((keyly & lobo)) """
         else:
             return False
-        lobo = lobo - {keycur}
-        if len(lobo) == 0:
-            return True
     return True

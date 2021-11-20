483cbfe0ec77dcdb6620b9a3e4204cc96631d9a5
Ran Kong <rkbriankong@gmail.com>
2021-11-19 23:26:17 -0600

    minimal operation setup and test

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+method that calculates the fewest number of operations needed
+to result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n):
+    copyi = 0
+    pastei = 0
+    """multiple paste"""
+    factory = 1
+    """edge case: 0 or 1"""
+    if n < 2:
+        return 0
+    """first factor"""
+    while (n % factory > 0):
+        factory += 1
+    while n % factory == 0:
+        copyi += 1
+        pastei += (factory - 1)
+        n = n / factory
+        if n == 1:
+            return copyi + pastei
+        while n % factory > 0:
+            factory += 1
+    return copyi + pastei

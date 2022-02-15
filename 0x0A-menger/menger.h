0b5be57ceb6df5a9e73f7b4547649526d6aee0d6
Ran Kong <rkbriankong@gmail.com>
2022-02-15 09:59:40 -0600

    setup

---
+++ 0x0A-menger/menger.h
@@ -0,0 +1,12 @@
+#ifndef MENGER_H
+#define MENGER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*major function*/
+void menger(int level);
+/*helper function*/
+int powerranger(int a);
+
+#endif /*MENGER_H*/

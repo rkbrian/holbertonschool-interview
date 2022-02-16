b2dd5e134f7baf32c2b2735fc03b5c6ae2127003
Ran Kong <rkbriankong@gmail.com>
2022-02-16 15:59:07 -0600

    recursive way to define regions and punch holes

---
+++ 0x0A-menger/0-menger.c
@@ -0,0 +1,62 @@
+#include "menger.h"
+
+/**
+ * menger - function that slides and merges an array of integers.
+ * @level: the level of the Menger Sponge to draw
+ */
+void menger(int level)
+{
+	int i, j, pow = powerranger(level);
+	char *line = NULL;
+
+	if (level < 0)
+		return;
+	line = malloc(sizeof(char) * pow);
+	if (!line)
+		return;
+	for (i = 0; i < pow; i++)
+	{
+		memset(line, '#', sizeof(char) * pow);
+		for (j = 0; j < pow; j++)
+		{
+			if (hollow_gram(i, j, pow))
+				line[j] = ' ';
+		}
+		printf("%s\n", line);
+	}
+	free(line);
+}
+
+/**
+ * powerranger - function to implement 3^a with given index a
+ * @a: ath power of 3
+ * Return: 3^a
+ */
+int powerranger(int a)
+{
+	int i = 0, ret = 1;
+
+	while (i < a)
+		ret *= 3, i++;
+	return (ret);
+}
+
+/**
+ * hollow_gram - function to define if a pair of coordinates in a matrix of
+ *  certain size should be hollow
+ * @i: vertical coordinate
+ * @j: horizontal coordinate
+ * @size: size of the square matrix
+ * Return: 0 for solid area, 1 for hollow area
+ */
+int hollow_gram(int i, int j, int size)
+{
+	int regional_i = i * 3 / size, regional_j = j * 3 / size;
+
+	if (j < 1)
+		return (0);
+	if (regional_i == 1 && regional_j == 1)
+		return (1);
+	i -= regional_i * size / 3, j -= regional_j * size / 3;
+	return (hollow_gram(i, j, size / 3));
+}
--- 0x0A-menger/menger.h
+++ 0x0A-menger/menger.h
@@ -2,11 +2,13 @@
 #define MENGER_H
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 /*major function*/
 void menger(int level);
 /*helper function*/
 int powerranger(int a);
+int hollow_gram(int a, int start, int end);
 
 #endif /*MENGER_H*/
